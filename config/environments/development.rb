e6ee213347e3d033078111106c7d626c92efb54c
Randall Lough <randallmlough@gmail.com>
2020-03-03 13:43:26 -0800

    don't check for same origin in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  # custom
+  config.action_controller.forgery_protection_origin_check = false
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
