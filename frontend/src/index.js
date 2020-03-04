0ccbd873dc6923554081917d83c91beccd49c4f2
Randall Lough <randallmlough@gmail.com>
2020-03-03 23:15:23 -0800

    reverting purging css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import configureStore from './store'
 import { SessionProvider } from './components/Session'
 import { BrowserRouter } from 'react-router-dom'
-import './tailwind.css'
+import './index.css'
 
 const initialState = {
   entities: {},
