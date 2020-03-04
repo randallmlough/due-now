74dec0d5c5a9bea01e2891e98b3ff5e6baac3903
Randall Lough <randallmlough@gmail.com>
2020-03-04 09:08:11 -0800

    add demo login details

--- frontend/src/views/auth/authenticate.js
+++ frontend/src/views/auth/authenticate.js
@@ -13,6 +13,24 @@ export default props => {
         <Redirect to="/" />
       ) : (
         <div className="container mx-auto px-4 py-32 h-screen">
+          <div className="w-100 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto mb-8">
+            <div className="bg-primary-100 bg-teal-100 border-primary-500 border-t-4 px-4 py-3 rounded-b shadow text-teal-900">
+              <div className="flex">
+                <div className="px-6">
+                  <p className="font-bold mb-3">Demo login</p>
+                  <ul className="mb-3">
+                    <li>
+                      <strong>Email:</strong> demo@example.com
+                    </li>
+                    <li>
+                      <strong>Password:</strong> 1234567
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="w-100 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto">
             <div className="bg-gray-100 px-10 pt-8 shadow-md w-full rounded-t">
               <div className="mb-6">
