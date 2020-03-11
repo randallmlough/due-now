9a33406743fa45da218770ccce05a4cd4b45ba51
Randall Lough <randallmlough@gmail.com>
2020-03-11 10:59:23 -0700

    removed unsued data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ InvoiceItem.destroy_all
 
 
 demo = User.create!(first_name: 'demo', last_name: 'account', email: 'demo@example.com', password: "1234567")
-10.times do
+30.times do
   t = Faker::Date.in_date_period
-  i = Invoice.new(created_by: demo.id, invoice_date: t,due_date: Faker::Date.between(from: t.days_ago(60), to: t), private: [true, false].sample, paid: [true,false].sample, payment_terms: ['net 30', 'net 15', 'net 21'].sample,recipient: {name: Faker::Name.name , email_address: Faker::Internet.email, mailing_address: Faker::Address.full_address, phone:Faker::PhoneNumber.phone_number})
+  i = Invoice.new(created_by: demo.id, invoice_date: t,due_date: Faker::Date.between(from: t.days_ago(60), to: t), private: [true, false].sample, paid: [true,false].sample, payment_terms: ['net 30', 'net 15', 'net 21'].sample, from: {name: 'Demo Account', email_address: 'demo@example.com', mailing_address: Faker::Address.full_address, phone:Faker::PhoneNumber.phone_number}, recipient: {name: Faker::Name.name , email_address: Faker::Internet.email, mailing_address: Faker::Address.full_address, phone:Faker::PhoneNumber.phone_number})
   i.invoiceable = demo
 
   if [true, false].sample
@@ -25,7 +25,6 @@ demo = User.create!(first_name: 'demo', last_name: 'account', email: 'demo@examp
     #{Faker::Beer.ibu}
     #{Faker::Beer.alcohol}"
   end
-
   i.save
   rand(1..4).times do
     rate =  Faker::Commerce.price
@@ -37,29 +36,29 @@ demo = User.create!(first_name: 'demo', last_name: 'account', email: 'demo@examp
 end
 
 
-5.times do
-  u = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "1234567", avatar: Faker::Avatar.image)
-  rand(1..4).times do
-    t = Faker::Date.in_date_period
-    i = Invoice.new(created_by: u.id, invoice_date: t,due_date: Faker::Date.between(from: t.days_ago(60), to: t), private: [true, false].sample, paid: [true,false].sample, payment_terms: ['net 30', 'net 15', 'net 21'].sample,recipient: {name: Faker::Name.name , email_address: Faker::Internet.email, mailing_address: Faker::Address.full_address, phone:Faker::PhoneNumber.phone_number})
-    i.invoiceable = u
+# 5.times do
+#   u = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "1234567", avatar: Faker::Avatar.image)
+#   rand(1..4).times do
+#     t = Faker::Date.in_date_period
+#     i = Invoice.new(created_by: u.id, invoice_date: t,due_date: Faker::Date.between(from: t.days_ago(60), to: t), private: [true, false].sample, paid: [true,false].sample, payment_terms: ['net 30', 'net 15', 'net 21'].sample,recipient: {name: Faker::Name.name , email_address: Faker::Internet.email, mailing_address: Faker::Address.full_address, phone:Faker::PhoneNumber.phone_number})
+#     i.invoiceable = u
 
-    if [true, false].sample
-      i.notes = "#{Faker::Beer.style} 
-      #{Faker::Beer.malts}
-      #{Faker::Beer.ibu}
-      #{Faker::Beer.alcohol}"
-    end
+#     if [true, false].sample
+#       i.notes = "#{Faker::Beer.style} 
+#       #{Faker::Beer.malts}
+#       #{Faker::Beer.ibu}
+#       #{Faker::Beer.alcohol}"
+#     end
 
-    i.save
-    rand(1..4).times do 
-      rate =  Faker::Commerce.price
-      qty = Faker::Number.between(from: 1, to: 6)
-      item = InvoiceItem.new(description: Faker::Beer.name, rate: rate, qty: qty, total: rate * qty)
-      item.invoice = i
-      item.save
-    end
+#     i.save
+#     rand(1..4).times do 
+#       rate =  Faker::Commerce.price
+#       qty = Faker::Number.between(from: 1, to: 6)
+#       item = InvoiceItem.new(description: Faker::Beer.name, rate: rate, qty: qty, total: rate * qty)
+#       item.invoice = i
+#       item.save
+#     end
     
-  end
-end
+#   end
+# end
 
