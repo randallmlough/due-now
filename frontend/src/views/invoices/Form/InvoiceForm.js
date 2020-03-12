4e6cbed6726d5216ccad770069725866f4e05d32
Randall Lough <randallmlough@gmail.com>
2020-03-12 12:45:10 -0700

    improved styling of date picker to match form

--- frontend/src/views/invoices/Form/InvoiceForm.js
+++ frontend/src/views/invoices/Form/InvoiceForm.js
@@ -33,12 +33,12 @@ export default function InvoiceForm({ invoice, setInvoice }) {
           </div>
           <div className="col md:w-1/2">
             <div className="md:flex md:items-center">
-              <div className="md:w-8/12">
+              <div className="md:w-3/5">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
                   Invoice No:
                 </label>
               </div>
-              <div className="md:w-4/12">
+              <div className="md:w-2/5">
                 <input
                   className="appearance-none border-b-2 border-transparent hover:border-primary-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-primary-300 text-right"
                   type="text"
@@ -49,28 +49,30 @@ export default function InvoiceForm({ invoice, setInvoice }) {
               </div>
             </div>
             <div className="md:flex md:items-center">
-              <div className="md:w-8/12">
+              <div className="md:w-3/5">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
                   Invoice Date:
                 </label>
               </div>
-              <div className="md:w-4/12">
+              <div className="md:w-2/5">
                 <DatePicker
                   selected={new Date(invoice.invoiceDate)}
                   onChange={handleDateChange('invoiceDate')}
+                  className="appearance-none border-b-2 border-transparent hover:border-primary-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-primary-300 text-right"
                 />
               </div>
             </div>
             <div className="md:flex md:items-center">
-              <div className="md:w-8/12">
+              <div className="md:w-3/5">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
                   Due Date:
                 </label>
               </div>
-              <div className="md:w-4/12">
+              <div className="md:w-2/5">
                 <DatePicker
                   selected={new Date(invoice.dueDate)}
                   onChange={handleDateChange('dueDate')}
+                  className="appearance-none border-b-2 border-transparent hover:border-primary-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-primary-300 text-right"
                 />
               </div>
             </div>
